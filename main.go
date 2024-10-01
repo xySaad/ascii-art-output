341de920f9daff11e1ee58d5c3779a5c98328509
srm <saadrm.dev@gmail.com>
2024-10-01 13:26:15 +0100

    initial commit

--- main.go
+++ main.go
@@ -7,27 +7,33 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	args := os.Args
+
+	if len(args) < 3 {
 		fmt.Fprintln(os.Stderr, "missing arguments OwO")
 	}
-	if len(os.Args) > 2 {
+	if len(args) > 3 {
 		fmt.Fprintln(os.Stderr, "too many arguments UwU")
 	}
 
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: go run . [STRING]")
+	if len(args) != 3 {
+		fmt.Fprintln(os.Stderr, "Usage: go run . [STRING] [BANNER]")
 		return
 	}
 
-	plainTxt, err := os.ReadFile("standard.txt")
+	plainTxt, err := os.ReadFile("./banners/" + args[2] + ".txt")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return
 	}
 	fileLines := strings.Split(string(plainTxt), "\n")
 
+	if len(fileLines) != 865 {
+		fmt.Fprintln(os.Stderr, "banner file", args[2], "has been modified and is invalid")
+		return
+	}
 	// Fetch the input from command-line arguments and clean it
-	userInput := cleanstring(os.Args[1])
+	userInput := cleanstring(args[1])
 
 	if len(userInput) == 0 {
 		return
